1fdaef579bae08069c296a4aa068f2076f7653f2
Jesse <jessebot.git@gmail.com>
2025-09-03 16:28:43 -0700

    build: add react.js + vite frontend

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
 	port := ":3000"
-	fmt.Println("Serving on", port)
+	staticFilesDir := filepath.Join("frontend", "dist")
+
+	http.Handle("/", http.FileServer(http.Dir(staticFilesDir)))
+
+	fmt.Printf("Serving on address %s", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
